dfbed9d6cfa6dd7b9163a7c8a5f1c9fbab2c40d9
harsh_vardhan <harsh10112001@gmail.com>
2024-11-08 23:29:12 +0530

    adding html page to registered page

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -7,14 +7,22 @@ hostRouter.get("/add-home", (req, res) => {
     res.sendFile(path.join(dirName, "/views/addHome.html"));
     });
 
-hostRouter.post("/add-home", (req, res, next) => {
-    res.send(`
-        <h1>Home registered successfully</h1>
-        <h2>House Name : ${req.body.housename}</h2>
-        <h2>Location : ${req.body.location}</h2>
-        <a href="/">Go To Home</a>
-    `);
-    next();
+// hostRouter.post("/add-home", (req, res, next) => {
+//     res.send(`
+//         <h1>Home registered successfully</h1>
+//         <h2>House Name : ${req.body.housename}</h2>
+//         <h2>Location : ${req.body.location}</h2>
+//         <a href="/">Go To Home</a>
+//     `);
+//     next();
+// });
+
+hostRouter.post("/add-home", (req, res) => {
+    res.redirect(`/host/registration?housename=${encodeURIComponent(req.body.housename)}&location=${encodeURIComponent(req.body.location)}`);
+});
+
+hostRouter.get("/registration", (req, res) => {
+    res.sendFile(path.join(dirName, "/views/registeredPage.html"));
 });
 
 module.exports = hostRouter;
